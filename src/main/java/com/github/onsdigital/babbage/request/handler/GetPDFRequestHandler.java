40e08564cccb5ab1c2712f97693a03161678f1f3
Rafa <rafael.garcia-pendon@ons.gov.uk>
2022-09-07 12:52:13 +0100

    Download Welsh PDFs

--- src/main/java/com/github/onsdigital/babbage/request/handler/GetPDFRequestHandler.java
+++ src/main/java/com/github/onsdigital/babbage/request/handler/GetPDFRequestHandler.java
@@ -5,9 +5,12 @@ import com.github.onsdigital.babbage.content.client.ContentFilter;
 import com.github.onsdigital.babbage.content.client.ContentReadException;
 import com.github.onsdigital.babbage.content.client.ContentResponse;
 import com.github.onsdigital.babbage.error.LegacyPDFException;
+import com.github.onsdigital.babbage.error.ResourceNotFoundException;
 import com.github.onsdigital.babbage.request.handler.base.BaseRequestHandler;
 import com.github.onsdigital.babbage.response.BabbageContentBasedBinaryResponse;
 import com.github.onsdigital.babbage.response.base.BabbageResponse;
+import com.github.onsdigital.babbage.util.RequestUtil;
+import com.github.onsdigital.babbage.util.ThreadContext;
 import com.github.onsdigital.babbage.util.json.JsonUtil;
 import org.apache.commons.lang3.StringUtils;
 
@@ -17,6 +20,7 @@ import java.util.Map;
 
 import static com.github.onsdigital.babbage.content.client.ContentClient.filter;
 import static com.github.onsdigital.logging.v2.event.SimpleEvent.error;
+import static com.github.onsdigital.logging.v2.event.SimpleEvent.warn;
 
 
 /**
@@ -25,14 +29,12 @@ import static com.github.onsdigital.logging.v2.event.SimpleEvent.error;
 public class GetPDFRequestHandler extends BaseRequestHandler {
 
     private static final String REQUEST_TYPE = "pdf";
-    public static final String CONTENT_TYPE = "application/pdf";
 
-
-    public BabbageResponse get(String requestedUri, HttpServletRequest requests) throws Exception {
-
+    @Override
+    public BabbageResponse get(String requestedUri, HttpServletRequest request) throws Exception {
         try {
-            return getPreGeneratedPDF(requestedUri);
-        } catch (ContentReadException e) {
+            return getPreGeneratedPDF(requestedUri, (String) ThreadContext.getData(RequestUtil.LANG_KEY));
+        } catch (ContentReadException | ResourceNotFoundException e) {
             error().exception(e).data("uri", requestedUri).log("pre-rendered PDF not found, throwing Legacy PDF error");
             throw new LegacyPDFException();
         }
@@ -43,9 +45,22 @@ public class GetPDFRequestHandler extends BaseRequestHandler {
         return REQUEST_TYPE;
     }
 
-    static BabbageResponse getPreGeneratedPDF(String requestedUri) throws ContentReadException, IOException {
-        ContentResponse contentResponse;
-        contentResponse = ContentClient.getInstance().getResource(requestedUri + "/page.pdf");
+    private static BabbageResponse getPreGeneratedPDF(String requestedUri, String language) throws ContentReadException, IOException {
+        ContentResponse contentResponse = null;
+        if ("cy".equals(language)) {
+            try {
+                contentResponse = ContentClient.getInstance().getResource(requestedUri + "/page_cy.pdf");
+            } catch(ResourceNotFoundException e) {
+                warn().exception(e).data("uri", requestedUri).log("pre-rendered Welsh PDF not found, using English version");
+                // There is no Welsh PDF, we'll serve the English version
+                contentResponse = null;
+            }
+        }
+
+        if (contentResponse == null) {
+            // Use the English version
+            contentResponse = ContentClient.getInstance().getResource(requestedUri + "/page.pdf");
+        }
         BabbageContentBasedBinaryResponse response = new BabbageContentBasedBinaryResponse(contentResponse, contentResponse.getDataStream(), contentResponse.getMimeType());
         String contentDispositionHeader = "attachment; ";
         contentDispositionHeader += contentResponse.getName() == null ? "" : "filename=\"" + getTitle(requestedUri) + "\"";
@@ -62,8 +77,9 @@ public class GetPDFRequestHandler extends BaseRequestHandler {
         String title = (String) descriptionMap.get("title");
         String edition = (String) descriptionMap.get("edition");
 
-        if (StringUtils.isNotEmpty(edition))
+        if (StringUtils.isNotEmpty(edition)) {
             title += " " + edition;
+        }
 
         title += ".pdf";
 
