1d4d0699117d7748529e1b03c210539b5317022b
cookel2 <eleanor.cook@ons.gov.uk>
2023-04-19 15:28:42 +0100

    refactor the MetricsFactory

--- src/main/java/com/github/onsdigital/babbage/metrics/MetricsFactory.java
+++ src/main/java/com/github/onsdigital/babbage/metrics/MetricsFactory.java
@@ -1,26 +1,39 @@
 package com.github.onsdigital.babbage.metrics;
 
+import static com.github.onsdigital.babbage.configuration.ApplicationConfiguration.appConfig;
+
 public class MetricsFactory {
 
-    private static CacheMetrics cacheMetrics;
+//    private static CacheMetrics cacheMetrics;
+    private static Metrics metrics;
 
     public static void init() throws Exception {
-        if (cacheMetrics != null) {
+//        if (cacheMetrics != null) {
+//            throw new Exception("Init already called");
+//        }
+
+        if (metrics != null) {
             throw new Exception("Init already called");
         }
 
-        cacheMetrics = new CacheMetrics();
+//        cacheMetrics = new CacheMetrics();
+        if (appConfig().babbage().getMetricsEnabled()) {
+            metrics = new CacheMetrics();
+        } else {
+            metrics = new NopMetricsImpl();
+        }
     }
 
     //Use getMetrics method to get object of type Metrics
     //If metrics are enabled then it will return the static object of type CacheMetrics
     //Or if metrics are not enabled then it will return an object of type NopMetricsImpl
     public static Metrics getMetrics(boolean metricsEnabled){
-        if (metricsEnabled) {
-            return MetricsFactory.cacheMetrics;
-        } else {
-            return new NopMetricsImpl();
-        }
+//        if (metricsEnabled) {
+//            return MetricsFactory.cacheMetrics;
+//        } else {
+//            return new NopMetricsImpl();
+//        }
+         return MetricsFactory.metrics;
     }
 
 }
