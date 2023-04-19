4d9cfa6c4ec203f661d4bb8c0677f447baa198bc
cookel2 <eleanor.cook@ons.gov.uk>
2023-04-19 12:49:20 +0100

    add NopMetricsImpl class

--- src/main/java/com/github/onsdigital/babbage/metrics/MetricsFactory.java
+++ src/main/java/com/github/onsdigital/babbage/metrics/MetricsFactory.java
@@ -8,39 +8,41 @@ import static com.github.onsdigital.babbage.configuration.ApplicationConfigurati
 
 public class MetricsFactory {
 
-    private static CacheMetrics metrics;
+    private static CacheMetrics cacheMetrics;
+    private static NopMetricsImpl nopMetrics;
 
     public static void init() throws Exception {
-        if (metrics != null) {
+        if (cacheMetrics != null) {
             throw new Exception("Init already called");
         }
 
-        metrics = new CacheMetrics();
+        cacheMetrics = new CacheMetrics();
+        nopMetrics = new NopMetricsImpl();
 
-        metrics.httpServer = new HTTPServer.Builder()
+        cacheMetrics.httpServer = new HTTPServer.Builder()
                 .withPort(appConfig().babbage().getMetricsPort())
                 .build();
 
-        metrics.publishDatePresent = Counter.build()
+        cacheMetrics.publishDatePresent = Counter.build()
                 .name("publish_date_present").help("Total requests for uris that have a past or future publishing date").register();
-        metrics.publishDateNotPresent = Counter.build()
+        cacheMetrics.publishDateNotPresent = Counter.build()
                 .name("publish_date_not_present").help("Total requests for uris that have no publishing date found").register();
-        metrics.publishDateInFuture = Counter.build()
+        cacheMetrics.publishDateInFuture = Counter.build()
                 .name("publish_date_in_future").help("Total requests for uris that have a future publishing date").register();
-        metrics.publishDateTooFarInPast = Counter.build()
+        cacheMetrics.publishDateTooFarInPast = Counter.build()
                 .name("publish_date_too_far_in_past").help("Total requests for uris that have a past publishing date too long ago (outside a given time span)").register();
-        metrics.cacheExpiryTime = Gauge.build()
+        cacheMetrics.cacheExpiryTime = Gauge.build()
                 .name("cache_expiry_time").help("The time until the cache expires and will be refreshed by another call to the server.").labelNames("is_greater_than_default").register();
     }
 
     //Use getMetrics method to get object of type Metrics
     //If metrics are enabled then it will return the static object of type CacheMetrics
-    //Or if metrics are not enabled then it will return an object of type NopMetricImpl
+    //Or if metrics are not enabled then it will return an object of type NopMetricsImpl
     public static Metrics getMetrics(boolean metricsEnabled){
         if (metricsEnabled) {
-            return MetricsFactory.metrics;
+            return MetricsFactory.cacheMetrics;
         } else {
-            return null;
+            return MetricsFactory.nopMetrics;
         }
     }
 
---
+++ src/main/java/com/github/onsdigital/babbage/metrics/NopMetricsImpl.java
@@ -0,0 +1,26 @@
+package com.github.onsdigital.babbage.metrics;
+
+import static com.github.onsdigital.logging.v2.event.SimpleEvent.info;
+
+public class NopMetricsImpl implements Metrics {
+
+    public void incPublishDatePresent() {
+        info().log("NopMetricsImpl incPublishDatePresent");
+    }
+
+    public void incPublishDateNotPresent() {
+        info().log("NopMetricsImpl incPublishDateNotPresent");
+    }
+
+    public void incPublishDateInFuture() {
+        info().log("NopMetricsImpl incPublishDateInFuture");
+    }
+
+    public void incPublishDateTooFarInPast() {
+        info().log("NopMetricsImpl incPublishDateTooFarInPast");
+    }
+
+    public void setCacheExpiryTime(Double expiryTime) {
+        info().log("NopMetricsImpl setCacheExpiryTime");
+    }
+}
