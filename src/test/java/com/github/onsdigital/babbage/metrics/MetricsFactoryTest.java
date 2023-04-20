b0c5cd9981e5721d33e55a373c9222415917d849
cookel2 <eleanor.cook@ons.gov.uk>
2023-04-20 17:48:04 +0100

    replace deprecated test code

--- src/test/java/com/github/onsdigital/babbage/metrics/MetricsFactoryTest.java
+++ src/test/java/com/github/onsdigital/babbage/metrics/MetricsFactoryTest.java
@@ -2,9 +2,10 @@ package com.github.onsdigital.babbage.metrics;
 
 import com.github.onsdigital.babbage.util.TestsUtil;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 
 public class MetricsFactoryTest {
@@ -12,27 +13,25 @@ public class MetricsFactoryTest {
     // Test target.
     private MetricsFactory metricsFactory;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
     @Before
     public void setup() throws Exception {
 //        MockitoAnnotations.initMocks(this);
-//        metricsFactory = new MetricsFactory();
+        metricsFactory = new MetricsFactory();
 //        TestsUtil.setPrivateStaticField(metricsFactory, "metrics", new NopMetricsImpl());
     }
 
     @Test
     public void testInitAlreadyCalled() throws Exception {
         //Given
-        metricsFactory = new MetricsFactory();
+//        metricsFactory = new MetricsFactory();
         TestsUtil.setPrivateStaticField(metricsFactory, "metrics", new NopMetricsImpl());
 
         // Then
-        expectedException.expect(Exception.class);
-        expectedException.expectMessage("Init already called");
-        metricsFactory.init();
-
+        Exception exception = assertThrows(Exception.class,
+                () -> metricsFactory.init());
+        assertTrue(exception.getMessage().contains("Init already called"));
     }
+
+
 
 }
